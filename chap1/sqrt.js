7a8492fda2684cf3a317b27775a29a63b6cd4ebd
Khanh <tran_khanh@outlook.com>
2018-09-20 21:32:34 +0700

    Factorial

--- chap1/sqrt.js
+++ chap1/sqrt.js
@@ -6,24 +6,47 @@ function average(x, y) {
     return (x + y) / 2;
 }
 
+// function sqrt(x) {
+//     const goodEnough = (guess, x) => {
+//         return Math.abs(square(guess) - x) < 0.001;
+//     }
+
+//     const improve = (guess, x) => {
+//         return average(guess, (x / guess));
+//     }
+
+//     const sqrtIter = (guess, x) => {
+//         if (goodEnough(guess, x)) {
+//             return guess;
+//         } else {
+//             return sqrtIter(improve(guess, x), x);
+//         }
+
+//     }
+//     return sqrtIter(1.0, x);
+// }
+
+// console.log(sqrt(9))
+
+//Apply lexical scope
 function sqrt(x) {
-    const goodEnough = (guess, x) => {
+    const goodEnough = (guess) => {
         return Math.abs(square(guess) - x) < 0.001;
     }
 
-    const improve = (guess, x) => {
+    const improve = (guess) => {
         return average(guess, (x / guess));
     }
 
-    const sqrtIter = (guess, x) => {
-        if (goodEnough(guess, x)) {
+    const sqrtIter = (guess) => {
+        if (goodEnough(guess)) {
             return guess;
         } else {
-            return sqrtIter(improve(guess, x), x);
+            return sqrtIter(improve(guess, x));
         }
 
     }
-    return sqrtIter(1.0, x);
+    return sqrtIter(1.0);
 }
 
 console.log(sqrt(9))
